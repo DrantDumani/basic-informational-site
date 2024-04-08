dfce3b61f162928a7dfb40b10134ea1fa47b057d
DrantDumani <darnjames@gmail.com>
2024-04-08 00:05:02 -0400

    Rewrite index.js using express

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const http = require("http");
 const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 8080;
 
-http
-  .createServer((req, res) => {
-    const url = req.url;
-    const filename = url === "/" ? "./index.html" : `.${url}.html`;
-    fs.readFile(filename, (err, data) => {
-      if (err) {
-        fs.readFile("./404.html", (_, data) => {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.write(data);
-          return res.end();
-        });
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
+const sendFile = (req, res, _) => {
+  const url = req.url;
+  const filename = url === "/" ? "./index.html" : `.${url}.html`;
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      fs.readFile("./404.html", (_, data) => {
+        res.writeHead(404, { "Content-Type": "text/html" });
         res.write(data);
         return res.end();
-      }
-    });
-  })
-  .listen("8080");
+      });
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    }
+  });
+};
+
+app.use(sendFile);
+
+app.listen(port, () => console.log(`Currently listening on port: ${port}`));
